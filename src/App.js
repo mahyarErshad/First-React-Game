5fd19daf2adeb48b3d4a371a2487a7994391b463
mahyarErshad <mahyar.ershad92@gmail.com>
2022-06-05 01:03:39 +0430

    just added the style file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+  <div className="main-ui">
+  <p className="problem"></p>
+
+  <form action="" className="our-form"/>
+    <input type="text" className="our-field" autoComplete="off"/>
+    <button>Submit</button>
+
+  <p className="status">You need <span className="points-needed">10</span> more points, and are allowed to make <span className="mistakes-allowed">2</span> more mistakes.</p>
+
+  <div className="progress">
+    <div className="boxes">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
     </div>
+    <div className="progress-inner"></div>
+  </div>
+
+</div>
+
+<div className="overlay">
+  <div className="overlay-inner">
+    <p className="end-message"></p>
+    <button className="reset-button">Start Over</button>
+  </div>
+</div>
+    </>
   );
 }
 
