43832795cc51a46851b6df92bb399372af59b74a
mahyarErshad <mahyar.ershad92@gmail.com>
2022-06-05 17:14:14 +0430

    just finished the job

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -3,6 +3,17 @@ import React from 'react'
 function Head({score , setScore, mistakes, setMistakes}) {
   const [currentProblem , setCurrentProblem] = React.useState(generateProblem());
   const [userAnswer , setUserAnswer] = React.useState('');
+  const [showError , setShowError] = React.useState(false);
+  const answerField = React.useRef(null);
+  const resetButton = React.useRef(null);
+
+  React.useEffect(()=>{
+    if (score === 10 || mistakes === 3){
+      setTimeout(()=> {
+        resetButton.current.focus();
+      }, 1000)
+    }
+  } , [score , mistakes]);
 
   function generateNumber(max) {
     return Math.floor(Math.random() * (max + 1))
@@ -17,13 +28,17 @@ function Head({score , setScore, mistakes, setMistakes}) {
   }
   function handleSubmit(e){
     e.preventDefault();
+    answerField.current.focus();
     let correctAnswer
+    if (userAnswer === "") return;
+    else{
     switch(currentProblem.operator){
       case '+': correctAnswer = currentProblem.numberOne + currentProblem.numberTwo; break;
       case '-': correctAnswer = currentProblem.numberOne - currentProblem.numberTwo; break;
       case 'x': correctAnswer = currentProblem.numberOne * currentProblem.numberTwo; break;
       case '÷': correctAnswer = currentProblem.numberOne / currentProblem.numberTwo; break;
       default: break;
+    }
     }
     if (correctAnswer === parseInt(userAnswer)){
       setScore(score + 1);
@@ -32,6 +47,8 @@ function Head({score , setScore, mistakes, setMistakes}) {
     } else{
       setMistakes(mistakes + 1);
       setUserAnswer('');
+      setShowError(true);
+      setTimeout(() => setShowError(false), 500);
     }
   }
   function resetGame (){
@@ -39,15 +56,16 @@ function Head({score , setScore, mistakes, setMistakes}) {
     setMistakes(0);
     setCurrentProblem(generateProblem());
     setUserAnswer('');
+    answerField.current.focus();
   }
   
 
   return (
     <>
     <div className={"main-ui" + (mistakes === 3 || score === 10 ? " blurred" : "")}>
-    <p className="problem">{currentProblem.numberOne} {currentProblem.operator} {currentProblem.numberTwo}</p>
+    <p className={"problem" + (showError === true ? " animate-wrong" : "")}>{currentProblem.numberOne} {currentProblem.operator} {currentProblem.numberTwo}</p>
     <form onSubmit={handleSubmit} action="" className="our-form">
-      <input value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} type="text" className="our-field" autoComplete="off"/>
+      <input ref={answerField} value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} type="text" className="our-field" autoComplete="off"/>
       <button type='submit'>Submit</button>
       </form>
     <p className="status">You need {10 - score} more points, and are allowed to make {2 - mistakes} more mistakes.</p>
@@ -55,7 +73,7 @@ function Head({score , setScore, mistakes, setMistakes}) {
     <div className={"overlay" + (score === 10 || mistakes === 3 ? " overlay--visible" : "")}>
   <div className="overlay-inner">
     <p className="end-message">{score === 10 ? "Well done. You won!" : "Sorry you lost the game!"}</p>
-    <button onClick={resetGame} className="reset-button">Start Over</button>
+    <button ref={resetButton} onClick={resetGame} className="reset-button">Start Over</button>
   </div>
 </div>
     </>
